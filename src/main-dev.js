dc2e3e3ef7cc9b3044e6ab9fd1b7b21ceaccfd06
aaron <aaron_shao2013@163.com>
2020-08-30 01:18:50 +0800

    依赖项以优化-发布和开发模式已区分开

--- src/main-dev.js
+++ src/main-dev.js
@@ -11,6 +11,8 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.use(VueQuillEditor)
 Vue.filter('dateFormat', function (originVal) {
@@ -28,8 +30,13 @@ Vue.config.productionTip = false
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // console.log(config)
+  return config
+})
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 new Vue({
